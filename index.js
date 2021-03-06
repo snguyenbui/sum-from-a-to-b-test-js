2adfd73ca760b85fdbe7d18994a4fcca4fbbf31b
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-06 00:57:22 +0000

    code implemented to recursively find sum

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN)
+  } else {
+    return toN;
+  }
 }
-
+console.log(sum(3, 7));
 module.exports = sum;
